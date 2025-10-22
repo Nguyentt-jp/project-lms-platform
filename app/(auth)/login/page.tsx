e16943581b5fd8ccf5dc5e7e7ddf4b93a0981136
Nguyen <nguyentt1503@gmail.com>
2025-10-22 15:34:35 +0700

    Setup Authentication and Database

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/app/(auth)/login/_components/login-form";
+import {auth} from "@/lib/auth";
+import {headers} from "next/headers";
+import {redirect} from "next/navigation";
+
+export default async function LoginPage() {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+    if (session) return redirect("/");
+    return <LoginForm/>
+}
